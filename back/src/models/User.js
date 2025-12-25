25f28c5760a10ccf88c3927b133d49c1bffea431
samrat10000 <trexgodmode777@gmail.com>
2025-12-25 18:12:21 +0530

    Fix: Remove next parameter from async User model pre-save middleware

--- back/src/models/User.js
+++ back/src/models/User.js
@@ -62,13 +62,12 @@ userSchema.virtual('tasks', {
  * LESSON: Middleware (Hooks)
  * Encrypt password using bcrypt before saving
  */
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
     if (!this.isModified('password')) {
-        return next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 /**
