701a3c5858d5cda10c4474a401a2ac68b0ed2d04
samrat10000 <trexgodmode777@gmail.com>
2025-12-25 18:03:55 +0530

    Fix: Prevent calling next() twice in User pre-save hook

--- back/src/models/User.js
+++ back/src/models/User.js
@@ -64,7 +64,7 @@ userSchema.virtual('tasks', {
  */
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) {
-        next();
+        return next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
